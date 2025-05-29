4683f9b207a2d7bafa67073e789053982c02423c
Nishu0105 <swetanishumishra@gmail.com>
2025-05-29 11:20:34 +0530

    updationUI

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
+    return render_template("home.html")
+
+@app.route("/check", methods=["GET"])
+def check():
     return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
